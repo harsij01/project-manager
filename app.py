77603404e23434074fd7913efa74c70f6386add8
harsij01 <harsinij6530@gmail.com>
2026-02-18 15:09:55 +0530

    added basic routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,3 +19,22 @@ class Task(db.Model):
     created_by = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     description = db.Column(db.String(300))
     status = db.Column(db.String(20), nullable=False, default="active")
+
+@app.route('/')
+def home():
+    return render_template('layout.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/kanban')
+def kanban():
+    return render_template('kanban.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
