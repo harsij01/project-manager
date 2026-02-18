629f0ba0572ad00719bdf078820f964cfef9fcc2
harsij01 <harsinij6530@gmail.com>
2026-02-18 14:12:09 +0530

    created batabase.db

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    role = db.Column(db.String(10), nullable=False)
+    password_hash = db.Column(db.String(300), nullable=False)
