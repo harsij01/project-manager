57a9b62a8387072c81de8a9b80aa8438d7d7df5e
harsij01 <harsinij6530@gmail.com>
2026-02-19 16:23:07 +0530

    added login section

--- app.py
+++ app.py
@@ -56,6 +56,10 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/project')
 def project():
     return render_template('project.html')
