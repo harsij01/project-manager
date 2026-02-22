c60785e8273fd2b0560925e0d468768f1d4babd2
harsij01 <harsinij6530@gmail.com>
2026-02-23 04:28:22 +0530

    fixed bug in datetime module import

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, flash, redirect, url_for, abo
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
 from functools import wraps
-import datetime
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'default_developement_key'
